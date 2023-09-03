b680f0ef027604ed55ae111e4449e1b59cb4a541
fthiagomedeiros <fthiagomedeiros@gmail.com>
2023-09-03 15:09:04 +0100

    Some testing for getting beans and query with entity manager

--- src/test/java/com/example/springtest/integration/SpringContextTest.java
+++ src/test/java/com/example/springtest/integration/SpringContextTest.java
@@ -3,10 +3,17 @@ package com.example.springtest.integration;
 import com.example.springtest.IntegrationTest;
 import com.example.springtest.PostgresContainer;
 import com.example.springtest.controllers.CustomerController;
+import com.example.springtest.domain.Customer;
 import com.example.springtest.repository.CustomerRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.jdbc.Sql;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,11 +24,21 @@ public class SpringContextTest extends PostgresContainer {
     private ApplicationContext context;
 
     @Test
+    @Sql("/scripts/CREATE_CUSTOMER.sql")
     public void contextLoads() {
         assertNotNull(context);
         String[] definitions = context.getBeanDefinitionNames();
         CustomerController customerController = (CustomerController) context.getBean("customerController");
         CustomerRepository customerRepository = (CustomerRepository) context.getBean("customerRepository");
         customerRepository.findAll();
+
+        DataSource dataSource = context.getBean(DataSource.class);
+        EntityManager manager = context.getBean(EntityManager.class);
+
+        List<?> q = manager.createQuery("SELECT c FROM Customer c").getResultList();
+        System.out.println(q);
+
+        //2 items has been found.
+        Assertions.assertEquals(2, q.size());
     }
 }
