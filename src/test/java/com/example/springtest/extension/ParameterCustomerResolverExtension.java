341bafb1e738983f53658f453e1f474eefd9c642
fthiagomedeiros <fthiagomedeiros@gmail.com>
2023-09-18 21:27:14 +0100

    JUnit 5 extensions use case for random values

---
+++ src/test/java/com/example/springtest/extension/ParameterCustomerResolverExtension.java
@@ -0,0 +1,56 @@
+package com.example.springtest.extension;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import com.example.springtest.domain.CustomerDTO;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class ParameterCustomerResolverExtension implements ParameterResolver {
+
+  public static final List<CustomerDTO> customers = List.of(
+      CustomerDTO.builder()
+          .id(UUID.fromString("5801fc28-716e-4619-9814-4ef74c7c8898"))
+          .username("johndoe1")
+          .firstName("John")
+          .lastName("Doe")
+          .cpf("00000000001")
+          .birth(LocalDateTime.of(2001, 1, 1, 1, 1))
+          .build(),
+
+      CustomerDTO.builder()
+          .id(UUID.fromString("5801fc28-716e-4619-9814-4ef74c7c8899"))
+          .username("johndoe1")
+          .firstName("John")
+          .lastName("Doe")
+          .cpf("00000000001")
+          .birth(LocalDateTime.now())
+          .build());
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target({PARAMETER})
+  public @interface RandomCustomer {
+
+  }
+
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext,
+      ExtensionContext extensionContext) throws ParameterResolutionException {
+    return parameterContext.isAnnotated(RandomCustomer.class);
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext,
+      ExtensionContext extensionContext) throws ParameterResolutionException {
+    return customers.get(ThreadLocalRandom.current().nextInt(0, customers.size()));
+  }
+}
--- src/test/java/com/example/springtest/repository/EntityManagerTest.java
+++ src/test/java/com/example/springtest/repository/EntityManagerTest.java
@@ -1,8 +1,12 @@
 package com.example.springtest.repository;
 
 import com.example.springtest.domain.Customer;
+import com.example.springtest.domain.CustomerDTO;
+import com.example.springtest.extension.ParameterCustomerResolverExtension;
+import com.example.springtest.extension.ParameterCustomerResolverExtension.RandomCustomer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -11,11 +15,13 @@ import org.springframework.test.context.jdbc.Sql;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest(properties = {
         "spring.test.database.replace = NONE",
         "spring.datasource.url = jdbc:tc:postgresql:15.3-alpine3.18:///springboot:CUSTOMER_INFO"
 })
+@ExtendWith(ParameterCustomerResolverExtension.class)
 public class EntityManagerTest {
 
     @Autowired
@@ -45,6 +51,12 @@ public class EntityManagerTest {
         //(already testes by the framework team)
         Customer customers = entityManager.find(Customer.class, UUID.fromString("5801fc28-716e-4619-9814-4ef74c7c8898"));
         assertEquals("Francisco", customers.getFirstName());
+    }
+
+
+    @Test
+    public void testJUnitExtension(@RandomCustomer CustomerDTO customer) {
+        assertNotNull(customer);
     }
 
 }
