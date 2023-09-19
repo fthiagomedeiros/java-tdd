a8939d80fdefeb2b390f56759af04c2bda6d43b8
Francisco Thiago <fthiagomedeiros@gmail.com>
2023-09-20 00:42:20 +0100

    Satisfying test dependency with Extensions (#3)

--- src/test/java/com/example/springtest/services/CustomerServiceTest.java
+++ src/test/java/com/example/springtest/services/CustomerServiceTest.java
@@ -7,36 +7,39 @@ import static org.mockito.Mockito.when;
 
 import com.example.springtest.domain.Customer;
 import com.example.springtest.domain.CustomerDTO;
+import com.example.springtest.extension.MapStructsResolver;
 import com.example.springtest.mapper.CustomerMapper;
 import com.example.springtest.repository.CustomerRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import lombok.Setter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
+@Setter
+@ExtendWith({MapStructsResolver.class, MockitoExtension.class})
 public class CustomerServiceTest {
 
   @Mock
   private CustomerRepository repository;
+
   private CustomerMapper mapper;
+
   @InjectMocks
-  private CustomerService customerService;
+  private CustomerService cut;
+
   private Customer customer;
 
   @BeforeEach
   public void setUp() {
     //work around to inject the mapper a service dependencies
-    mapper = Mappers.getMapper(CustomerMapper.class);
-    customerService = new CustomerService(repository, mapper);
-
+    cut = new CustomerService(repository, mapper);
     customer = Customer.builder()
         .id(UUID.randomUUID())
         .firstName("Francisco")
@@ -48,22 +51,20 @@ public class CustomerServiceTest {
         .build();
   }
 
-  @DisplayName("Testing for the entity customer")
+  @DisplayName("Testing for the entity customer and extensions")
   @Test
   void firstTestValidation() {
-    System.out.println(customer);
     //In fact there is no reason to test this method, once all this code is generated automatically
     //by MapStructs and mocking the response from JpaRepository
     when(repository.findAll()).thenReturn(List.of(customer));
 
     //Fetch all customers
-    List<CustomerDTO> items = customerService.getAllCustomers();
+    List<CustomerDTO> items = cut.getAllCustomers();
     assertThat(items.size()).isEqualTo(1);
     assertThat(items.size()).isNotNull();
 
     //Check number of invocations
     verify(repository, times(1)).findAll();
-
   }
 
 }
