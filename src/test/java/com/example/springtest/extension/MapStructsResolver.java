f554b7e4cd177f2afd4253d6cbed684c6a3696cc
fthiagomedeiros <fthiagomedeiros@gmail.com>
2023-09-20 00:37:19 +0100

    Satisfying test dependency with Extensions

---
+++ src/test/java/com/example/springtest/extension/MapStructsResolver.java
@@ -0,0 +1,18 @@
+package com.example.springtest.extension;
+
+import com.example.springtest.mapper.CustomerMapper;
+import com.example.springtest.services.CustomerServiceTest;
+import java.util.List;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.mapstruct.factory.Mappers;
+
+public class MapStructsResolver implements BeforeEachCallback {
+
+  @Override
+  public void beforeEach(ExtensionContext context) throws Exception {
+    List<Object> testInstances = context.getRequiredTestInstances().getAllInstances();
+    ((CustomerServiceTest) testInstances.get(0))
+        .setMapper(Mappers.getMapper(CustomerMapper.class));
+  }
+}
